59f38df58a02d66748104de4bd766dc6ada9cba5
itsumura-h <dumblepy@gmail.com>
2019-04-19 17:31:09 +0900

    add react build dir

---
+++ project/admin/templates/admin/build/precache-manifest.3c4260894b0b37c4ff98f1726b3fb990.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "b0abcf536cbd3fb1dbe3",
+    "url": "/static/js/main.661c3f9e.chunk.js"
+  },
+  {
+    "revision": "4bebc0d41a2d62292baf",
+    "url": "/static/js/2.b96a2331.chunk.js"
+  },
+  {
+    "revision": "b0abcf536cbd3fb1dbe3",
+    "url": "/static/css/main.0d635c4e.chunk.css"
+  },
+  {
+    "revision": "ad0c476cf8fcaf274835589e0db6ecbd",
+    "url": "/index.html"
+  }
+];
