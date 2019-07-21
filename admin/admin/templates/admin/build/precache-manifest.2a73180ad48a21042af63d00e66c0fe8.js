840fad298e6489bccc0578a2524c075ef3eb6ab9
itsumura-h <dumblepy@gmail.com>
2019-07-22 04:08:25 +0900

    add build package

---
+++ admin/admin/templates/admin/build/precache-manifest.2a73180ad48a21042af63d00e66c0fe8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fbe2def6a0104ec82c88",
+    "url": "/static/js/main.130f5f4d.chunk.js"
+  },
+  {
+    "revision": "eedf58d48627175932b5",
+    "url": "/static/js/2.ef4045e2.chunk.js"
+  },
+  {
+    "revision": "fbe2def6a0104ec82c88",
+    "url": "/static/css/main.395f2b23.chunk.css"
+  },
+  {
+    "revision": "8ea0181cdb8f9537faf8d58593aa358b",
+    "url": "/index.html"
+  }
+];
